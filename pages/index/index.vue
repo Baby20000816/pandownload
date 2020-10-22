7b44f88f9b51a9715f1bbebd8129b2875f21f743
unknown <1273163614@qq.com>
2020-10-22 14:38:28 +0800

    全选和取消全选的实现

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -20,9 +20,9 @@
 				</template>
 			</template>
 			<template v-else>
-				<view slot='left' class="font-md ml-3 text-primary">取消</view>
+				<view slot='left' class="font-md ml-3 text-primary" @click="handleCheckAll(false)">取消</view>
 				<text class="font-md font-weight-bold">已选中{{checkCount}}个</text>
-				<view slot='right' class="font-md mr-3 text-primary">全选</view>
+				<view slot='right' class="font-md mr-3 text-primary"  @click="handleCheckAll(true)">全选</view>
 			</template>
 		</nav-bar>
 		<view class="px-3 py-2">
@@ -92,6 +92,11 @@ export default {
 		},
 		select(e) {
 			this.lists[e.index].checked = e.value;
+		},
+		handleCheckAll(checked){
+			this.lists.forEach(item=>{
+				item.checked = checked;
+			})
 		}
 	}
 };
